34bcf767b2a8e43c5636a356c2db93d54211c808
gaodeying <gaodeying@meituan.com>
2019-08-15 14:12:17 +0800

    FlutterWithNative

---
+++ flutter_to_native/plugins/flutter_plugin_batterylevel/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flutter_plugin_batterylevel/FlutterPluginBatterylevelPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterPluginBatterylevelPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterPluginBatterylevelPlugin"]];
+}
+
+@end
