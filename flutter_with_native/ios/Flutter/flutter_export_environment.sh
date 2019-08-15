e6948434b032702c83effb4ca59ec1fb3a0ac200
gaodeying <gaodeying@meituan.com>
2019-08-15 14:28:05 +0800

    Flutter

---
+++ flutter_with_native/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/gaodeying/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gaodeying/Desktop/Git_FlutterHybrid/flutter_with_native"
+export "FLUTTER_TARGET=/Users/gaodeying/Desktop/Git_FlutterHybrid/flutter_with_native/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/gaodeying/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
